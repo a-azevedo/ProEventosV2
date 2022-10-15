bc7bea0af20ca29f07f12165e4378cb46a8f359b
a-azevedo <aelmajan.azevedo@gmail.com>
2022-10-15 12:58:55 -0300

    varias alterações como modificações no gitignore, adicionadas as bibliotecas do bootstrap, ngx-bootstrap, fontawesome, criacao de 2 componentes e comunicação do front com o back da aplicação resgatando informações sobre os eventos

---
+++ Back/src/ProEventosV2.API/Controllers/EventosController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using ProEventosV2.API.Data;
+using ProEventosV2.API.Models;
+
+namespace ProEventosV2.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventosController : ControllerBase
+{ 
+    private readonly DataContext context;
+
+    public EventosController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return context.Eventos;
+    }
+
+    [HttpGet("{id}")]
+    public Evento GetById(int id)
+    {
+        return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "http post method exemple";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return $"http put method exemple com id = {id}";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return $"http delete method exemple com id = {id}";
+    }
+}
--- Back/src/ProEventosV2.API/Program.cs
+++ Back/src/ProEventosV2.API/Program.cs
@@ -9,7 +9,8 @@ builder.Services.AddDbContext<DataContext>(
     context => context.UseSqlite(builder.Configuration.GetConnectionString("Default"))
 );
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//adiciona recurso de compartilhamento de origem cruzada
+builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,6 +26,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+//Configurando as políticas de permissões do CORS
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.MapControllers();
 
